d66895084ced4e7a0a612ec14a38e60d3a7f9d7d
Bruno Fonseca <bruno-fonseca@outlook.com>
2019-12-19 13:45:59 -0300

    Leitura e sincronizacao dos lotes

--- app/Http/Controllers/ClienteLoteController.php
+++ app/Http/Controllers/ClienteLoteController.php
@@ -185,7 +185,133 @@ class ClienteLoteController extends Controller
 
         }
     public function sincronizarLote($loteId){
-        echo $loteId;
-        die;
+
+        $lote     = ClienteLote::find($loteId);
+        $produtos = $lote->produtos;
+
+        echo "Quantidade de Itens do Lote : ".count($produtos);
+        echo "<br/>";
+
+        foreach ($produtos as $index => $produto) {
+
+            echo "Indice :". $index;
+            echo "<br/>";
+            echo "Gtin :".$produto->gtin;
+            echo "<br/>";
+
+            $produtoBC = DB::select("SELECT
+                                                bcp.*,
+                                                bcp.nome as base_comparativa_nome,
+                                                bcgtin.gtin as base_comparativa_gtin,
+                                                pcicms.aliquota as base_comparativa_icms_aliquota,
+                                                pcicms.possui_st as base_comparativa_icms_possui_st,
+                                                pccofins.aliquota as base_comparativa_cofins_aliquota,
+                                                pccofins.cst as base_comparativa_cofins_cst,
+                                                pcpis.aliquota as base_comparativa_pis_aliquota,
+                                                pcpis.cst as base_comparativa_pis_cst
+                                            FROM bc_produto_gtin AS bcgtin
+                                               INNER JOIN bc_produto AS bcp ON bcp.id = bcgtin.bc_produto_fk_id
+                                               LEFT JOIN bc_perfil_contabil pc ON pc.ncm_fk_id = bcp.ncm_fk_id
+                                               LEFT JOIN bc_perfil_contabil_icms pcicms ON pcicms.bc_perfil_contabil_fk_id = pc.id
+                                               LEFT JOIN bc_perfilcontabil_cofins pccofins ON pccofins.bc_perfil_contabil_fk_id = pc.id
+                                               LEFT JOIN bc_perfilcontabil_pis pcpis ON pcpis.bc_perfil_contabil_fk_id = pc.id
+                                            WHERE bcgtin.gtin = '{$produto->gtin}' AND pc.trib_estab_origem_fk_id = {$lote->cliente->enquadramento_tributario_fk_id} ");
+
+            // Verifica se o produto está na base comparativa
+            if(isset($produtoBC[0])){
+                echo "GTIN ".$produto->gtin.' -  encontrado na base comparativa';
+                echo "<br/>";
+            }else{
+
+                // Se caso o produto não existir na base comparativa , tenta uma consulta no cosmos
+
+                $url = 'https://api.cosmos.bluesoft.com.br/gtins/'.$produto->gtin.'.json';
+
+                $headers = array(
+                    "Content-Type: application/json",
+                    "X-Cosmos-Token: SJaFhcrcDrvFrwch5xPQvw"
+                );
+
+                $curl = curl_init($url);
+
+                curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+                curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+                curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($curl, CURLOPT_FAILONERROR, true);
+
+                $data = curl_exec($curl);
+
+                if ($data === false || $data == NULL) {
+
+                    // Na impossibilidade de encontrar o produto no cosmos , ele tenta o ultimo local que é a Base Comparativa Auxiliar.
+
+                    $produtoBCAux = BCProdutoAux::where('gtin_fk_id','=',$produto->gtin)->first();
+
+                    if(empty($produtoBCAux)){
+                        echo "GTIN ".$produto->gtin.' não encontrado em nenhuma das bases.';
+                        echo "<br/>";
+                    }else{
+                        echo "GTIN ".$produto->gtin.' encontrado na base auxiliar:';
+                        echo "<br/>";
+//                        echo "<pre>";
+//                        print_r($produtoBCAux);
+//                        echo "<br/>";
+                    }
+
+                    //var_dump(curl_error($curl));
+
+                } else {
+
+                    $object = json_decode($data);
+
+                    if(is_object($object)){
+
+                        echo "GTIN ".$produto->gtin.' encontrado no cosmos.';
+                        echo "<br/>";
+//                        echo "<pre>";
+//                        print_r($object);
+//                        echo "<br/>";
+//
+//                        try {
+//
+//                            $gtin  = BCProdutoGtin::where('gtin','=',$produto->gtin)->get();
+//
+//                            if(count($gtin) == 0){
+//
+//                                $newProdutoBC = BCProduto::create([
+//                                    'status'        => '',
+//                                    'nome'          => "$object->description",
+//                                    'descricao'     => "$object->description",
+//                                    'preco_medio'   => "$object->avg_price",
+//                                    'preco_maximo'  => "$object->max_price",
+//                                    'thumbnail'     => "$object->thumbnail",
+//                                    'altura'        => "$object->height",
+//                                    'largura'       => "$object->width",
+//                                    'comprimento'   => "$object->length",
+//                                    'peso_liquido'  => "$object->net_weight",
+//                                    'cest_fk_id'    => isset($object->cest->code) ? $object->cest->code : 1 ,
+//                                    'gpc_fk_id'     => isset($object->gpc->code) ? $object->gpc->code : 1,
+//                                    'ncm_fk_id'     => isset($object->ncm->code) ? $object->ncm->code : 1
+//                                ]);
+//
+//                                $newProdutoBCGtin = BCProdutoGtin::create([
+//                                    'gtin'             => $object->gtin,
+//                                    'bc_produto_fk_id' => $newProdutoBC->id
+//                                ]);
+//                            }
+//
+//                        }catch (\PDOException $e){
+//                            echo $e->getMessage();
+//                            die;
+//                        }
+                    }
+                }
+
+                curl_close($curl);
+
+            }
+            echo "--------------------------------------------------------------------------------------------------------------------";
+            echo "<br/>";
+        }
     }
 }
