92e02b62478485ba1633b1c37013d0be89849528
Bruno Fonseca <bruno-fonseca@outlook.com>
2020-01-14 17:24:47 -0300

    Atualização/criação de NCM de um produto

--- app/Http/Controllers/BCProdutoController.php
+++ app/Http/Controllers/BCProdutoController.php
@@ -55,8 +55,9 @@ class BCProdutoController extends Controller
              $produto->save();
 
              $ncm = BCProdutoNcm::where('bc_produto_fk_id',$request->get('bc_produto_fk_id'))->updateOrCreate([
-                 'inicio'    => date('Y-m-d'),
-                 'ncm_fk_id' => $ncm
+                 'inicio'           => date('Y-m-d'),
+                 'ncm_fk_id'        => $ncm,
+                 'bc_produto_fk_id' => $produto->id
              ]);
 
            }catch(\Exception $e){
