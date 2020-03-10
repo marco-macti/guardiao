65c5b4270f1f282bdd76594133a20cb37159228e
Bruno Fonseca <bruno-fonseca@outlook.com>
2020-03-10 12:11:46 -0300

    Fix : ajustes gerais no codigo

--- app/Http/Controllers/ClienteLoteController.php
+++ app/Http/Controllers/ClienteLoteController.php
@@ -800,6 +800,7 @@ class ClienteLoteController extends Controller
 //                        ]);
 
                         $lastInsertId = DB::SELECT ("SELECT lp.id  FROM public.lote_produto lp ORDER BY lp.id DESC OFFSET 0 LIMIT 1");
+                        $lastInsertId = (int) $lastInsertId + 1;
 
 //                        LoteProduto::create([
 //                            'id'                       =>  $lastInsertId[0]->id ++,
@@ -823,7 +824,7 @@ class ClienteLoteController extends Controller
 
                         echo "<pre>";
                         print_r([
-                            'id'                       =>  $lastInsertId[0]->id ++,
+                            'id'                       =>  $lastInsertId,
                             'gtin'                      => "$linha[2]",
                             'seu_codigo'                => "$linha[0]",
                             'seu_nome'                  => '$linha[1]',
